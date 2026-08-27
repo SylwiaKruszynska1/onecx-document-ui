4b366963ec49d3f33737feb040bc41a2dcc369b4
SylwiaKruszynska1 <221315718+SylwiaKruszynska1@users.noreply.github.com>
2026-08-27 14:14:52 +0200

    fix: remove unnecesary imports

--- src/app/document/document.module.ts
+++ src/app/document/document.module.ts
@@ -1,25 +1,9 @@
 import { NgModule } from '@angular/core'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
-import { LetDirective } from '@ngrx/component'
 import { EffectsModule } from '@ngrx/effects'
 import { StoreModule } from '@ngrx/store'
-import { TranslateModule } from '@ngx-translate/core'
 
-import { ButtonModule } from 'primeng/button'
-import { DatePickerModule } from 'primeng/datepicker'
-import { SelectModule } from 'primeng/select'
-import { InputTextModule } from 'primeng/inputtext'
-import { MultiSelectModule } from 'primeng/multiselect'
-import { StepsModule } from 'primeng/steps'
-import { TableModule } from 'primeng/table'
-import { TooltipModule } from 'primeng/tooltip'
-import { DialogModule } from 'primeng/dialog'
-import { CheckboxModule } from 'primeng/checkbox'
-import { Textarea } from 'primeng/inputtextarea'
-import { TabsModule } from 'primeng/tabs'
-
-import { AngularAcceleratorModule, providePortalDialogService } from '@onecx/angular-accelerator'
+import { providePortalDialogService } from '@onecx/angular-accelerator'
 
 import { documentFeature } from './document.reducers'
 import { routes } from './document.routes'
@@ -28,19 +12,9 @@ import { DocumentDetailsEffects } from './pages/document-details/document-detail
 import { DocumentCreateOperationsEffects } from './operations/document-create-operations.effects'
 import { DocumentCreateComponent } from './pages/document-create/document-create.component'
 import { DocumentCreateEffects } from './pages/document-create/document-create.effects'
-import { DocumentCreateAttachmentsComponent } from './pages/document-create/components/document-create-attachments/document-create-attachments.component'
-import { DocumentCreateCharacteristicsComponent } from './pages/document-create/components/document-create-characteristics/document-create-characteristics.component'
-import { DocumentCreateDetailsFormComponent } from './pages/document-create/components/document-create-details-form/document-create-details-form.component'
-import { DocumentQuickUploadFormComponent } from './pages/document-quick-upload/document-quick-upload-form/document-quick-upload-form.component'
 import { DocumentQuickUploadComponent } from './pages/document-quick-upload/document-quick-upload.component'
-import { DocumentSearchCriteriaComponent } from './pages/document-search/components/document-search-criteria/document-search-criteria.component'
 import { DocumentSearchComponent } from './pages/document-search/document-search.component'
 import { DocumentSearchEffects } from './pages/document-search/document-search.effects'
-import { DocumentDetailsAttachmentListComponent } from './pages/document-details/components/document-details-attachment-list/document-details-attachment-list.component'
-import { FileUploadComponent } from './components/file-upload/file-upload.component'
-import { DocumentDetailsFormComponent } from './components/document-details-form/document-details-form.component'
-import { DocumentDetailsCharacteristicsComponent } from './pages/document-details/components/document-details-characteristics/document-details-characteristics.component'
-import { RetryFileUploadDialogComponent } from './pages/document-details/dialog/retry-file-upload-dialog/retry-file-upload-dialog.component'
 import { DocumentTypeSearchComponent } from './pages/document-type-search/document-type-search.component'
 import { DocumentTypeSearchEffects } from './pages/document-type-search/document-type-search.effects'
 
@@ -48,37 +22,11 @@ import { DocumentTypeSearchEffects } from './pages/document-type-search/document
   providers: [providePortalDialogService()],
   imports: [
     DocumentCreateComponent,
-    DocumentCreateDetailsFormComponent,
-    DocumentCreateAttachmentsComponent,
-    DocumentCreateCharacteristicsComponent,
     DocumentDetailsComponent,
     DocumentSearchComponent,
-    DocumentSearchCriteriaComponent,
     DocumentQuickUploadComponent,
-    DocumentQuickUploadFormComponent,
-    DocumentDetailsFormComponent,
-    DocumentDetailsAttachmentListComponent,
-    DocumentDetailsCharacteristicsComponent,
-    FileUploadComponent,
-    RetryFileUploadDialogComponent,
     DocumentTypeSearchComponent,
-    LetDirective,
-    AngularAcceleratorModule,
     RouterModule.forChild(routes),
-    FormsModule,
-    ReactiveFormsModule,
-    ButtonModule,
-    DatePickerModule,
-    SelectModule,
-    InputTextModule,
-    MultiSelectModule,
-    StepsModule,
-    TableModule,
-    TabsModule,
-    TooltipModule,
-    Textarea,
-    DialogModule,
-    CheckboxModule,
     StoreModule.forFeature(documentFeature),
     EffectsModule.forFeature([
       DocumentCreateEffects,
@@ -86,8 +34,7 @@ import { DocumentTypeSearchEffects } from './pages/document-type-search/document
       DocumentSearchEffects,
       DocumentCreateOperationsEffects,
       DocumentTypeSearchEffects
-    ]),
-    TranslateModule
+    ])
   ]
 })
 export class DocumentModule {}
